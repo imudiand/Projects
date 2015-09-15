3b7b830c5b6d10e81621e1d8d387f261ad39d33b
himudianda <himudianda@ubuntu.(none)>
2015-09-15 09:49:00 -0700

    Templates

--- coffeeshop/web/views.py
+++ coffeeshop/web/views.py
@@ -1,6 +1,18 @@
 from web import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello World !"
+	user = {'nickname': 'Harshit'}  # fake user
+	reviews = [  # fake array of reviews
+		{ 
+			'author': {'nickname': 'John'}, 
+			'body': 'Beautiful ambience & great tasting coffee' 
+		},
+		{ 
+			'author': {'nickname': 'Susan'}, 
+			'body': 'Top class customer service !' 
+		}
+	]
+	return render_template('index.html', title="Imudianda Coffeeshop", user=user, reviews=reviews)
